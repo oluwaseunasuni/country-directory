e0910f12c7f7f491fa3eddd6818cd5913347330e
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-08-14 02:02:34 +0100

    added bootstrap select field

--- src/components/Request.vue
+++ src/components/Request.vue
@@ -3,59 +3,46 @@
     <div class="Container">
 
       <label for="country">Select a Country: </label>
-      <select
-        v-model="selectedCountry"
-        @change="pickedCountry(selectedCountry)"
-        
-        
-        >
+      <b-form-select 
+        v-model="selectedOne" 
+        @change="pickedCountry(selectedOne)"
+        class="mb-3">
 
-        <option 
+        <b-form-select-option 
           v-for="country in countries"
           :key="country.id"
           :label="country.name"
           :value="country.id">
-          
-        </option>
-      </select>
+        </b-form-select-option>
+      </b-form-select>
 
-      <div>
+      <label for="state">Select a State: </label>
+      <b-form-select 
+        v-model="selectedTwo" 
+        @change="pickedState(selectedTwo)"
+        class="mb-3">
 
-        <label for="state">Select a State: </label>
-        <select 
-          @change="pickedState(selectedState)"
-          v-model="selectedState"
-          aria-placeholder="Select a State"
-          >
+        <b-form-select-option 
+          v-for="state in states"
+          :key="state.id"
+          :label="state.name"
+          :value="state.id">
+        </b-form-select-option>
+      </b-form-select>
 
-          <option 
-            v-for="state in states"
-            :key="state.id"
-            :label="state.name"
-            :value="state.id">
-            
-          </option>
-        </select>
-      </div>
-      
-      <div>
+      <label for="city">Select a City: </label>
+      <b-form-select 
+        v-model="selectedThree" 
+        @change="pickedCity(selectedThree)"
+        class="mb-3">
 
-        <label for="city">Select a City: </label>
-        <select 
-          @change="pickedCity(selectedCity)"
-          v-model="selectedCity"
-          aria-placeholder="Select a City"
-          >
-
-          <option 
-            v-for="city in cities"
-            :key="city.id"
-            :label="city.name"
-            :value="city.id">
-            
-          </option>
-        </select>
-      </div>
+        <b-form-select-option 
+          v-for="city in cities"
+          :key="city.id"
+          :label="city.name"
+          :value="city.id">
+        </b-form-select-option>
+      </b-form-select>
 
     </div>
   </div>
@@ -91,20 +78,20 @@ export default {
   },
 
   methods: {
-    pickedCountry(selectedCountry) {
-      const country = this.countries.find((e) => e.id == selectedCountry);
+    pickedCountry(selectedOne) {
+      const country = this.countries.find((e) => e.id == selectedOne);
       this.states = country.states;
       eventBus.$emit("selected-country", country.name);
     },
 
-    pickedState(selectedState) {
-      const state = this.states.find((e) => e.id == selectedState);
+    pickedState(selectedTwo) {
+      const state = this.states.find((e) => e.id == selectedTwo);
       this.cities = state.cities;
       eventBus.$emit("selected-state", state.name)
     },
 
-    pickedCity(selectedCity) {
-      const city = this.cities.find((e) => e.id == selectedCity);
+    pickedCity(selectedThree) {
+      const city = this.cities.find((e) => e.id == selectedThree);
       eventBus.$emit("selected-city", city.name);
     }
   }
